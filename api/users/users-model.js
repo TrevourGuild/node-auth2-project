8c5ab2eb1bd3426c271b5f145b92b68d1da2ce18
TrevourGuild <trevorr.guildd@gmail.com>
2021-09-14 14:57:08 -0700

    finished register endpoint

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -17,11 +17,18 @@ function find() {
         "role_name": "instructor"
       }
     ]
+          select
+              user_id,
+              username,
+              role_name
+      from users
+      join roles on 
+          users.role_id = roles.role_id
    */
-  
-    return db('users as u')
-      .join('roles as r', 'u.role', '=', 'r.id')
-      .select('u.id', 'u.username', 'r.name as role')
+
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'role_name')
 
 }
 
@@ -38,10 +45,19 @@ function findBy(filter) {
         "role_name": "admin",
       }
     ]
+          select
+              user_id,
+              username,
+              password,
+              role_name
+      from users
+      join roles on 
+          users.role_id = roles.role_id
+      where users.user_id = 1
    */
-  return db('users as u')
-    .join('roles as r', 'u.role', '=', 'r.id')
-    .select('u.id', 'u.username', 'r.name as role', 'u.password')
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
     .where(filter)
 }
 
@@ -56,11 +72,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
-  return db('users as u')
-  .join('roles as r', 'u.role', '=', 'r.id')
-  .select('u.id', 'u.username', 'r.name as role')
-  .where('u.id', user_id)
-  .first()
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
